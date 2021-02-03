11cb05fc88faa054fa1ac097b0c9c64b0992ce4e
John Anderton <janderton24@gmail.com>
2021-02-03 14:18:12 -0700

    adds two pointer solution to the linkedListCycle

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -13,3 +13,24 @@ Stats:
 Runtime: 220 ms, faster than 6.04% of JavaScript online submissions for Linked List Cycle.
 Memory Usage: 48.9 MB, less than 5.19% of JavaScript online submissions for Linked List Cycle.
 */
+
+//Two pointer solution
+
+var hasCycle = function(head) {
+  if (!head) { return [] }
+  function run(fast, slow) {
+      if (!fast.next || !fast.next.next) { return false }
+      if (fast.next === slow) {
+          return true
+      }
+      return run(fast.next.next, slow.next);
+  }
+
+  return run(head, head);
+};
+
+/*
+Stats:
+Runtime: 100 ms, faster than 20.37% of JavaScript online submissions for Linked List Cycle.
+Memory Usage: 41.4 MB, less than 25.59% of JavaScript online submissions for Linked List Cycle.
+*/
