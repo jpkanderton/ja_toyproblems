b46ab3db6040a082a8be1e935d1c4ccc7305f565
John Anderton <janderton24@gmail.com>
2021-02-04 10:49:10 -0700

    adds stats to working solution

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -6,3 +6,9 @@ var reverseList = function(head) {
   }
   return result;
 };
+
+/*
+Stats:
+Runtime: 108 ms, faster than 10.78% of JavaScript online submissions for Reverse Linked List.
+Memory Usage: 40.8 MB, less than 17.79% of JavaScript online submissions for Reverse Linked List.
+*/
