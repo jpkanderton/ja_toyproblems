170c7103ec2af9fe928e79915db7b8d201891e08
John Anderton <janderton24@gmail.com>
2021-02-05 12:17:29 -0700

    adds additional solution using a set with a runtime of 96ms

--- linkedListComponents.js
+++ linkedListComponents.js
@@ -17,3 +17,25 @@ Stats:
 Runtime: 416 ms, faster than 6.94% of JavaScript online submissions for Linked List Components.
 Memory Usage: 41.4 MB, less than 98.61% of JavaScript online submissions for Linked List Components.
 */
+
+var numComponents = function(head, G) {
+  let curr, next;
+  var set = new Set();
+  G.forEach(item => set.add(item));
+
+  let total = 0;
+
+  while (head) {
+      curr = set.has(head.val);
+      next = (head.next) ? set.has(head.next.val) : false;
+      if (curr && !next) total++;
+      head = head.next;
+  }
+  return total;
+};
+
+/*
+Stats:
+Runtime: 96 ms, faster than 62.50% of JavaScript online submissions for Linked List Components.
+Memory Usage: 43.7 MB, less than 37.50% of JavaScript online submissions for Linked List Components.
+*/
