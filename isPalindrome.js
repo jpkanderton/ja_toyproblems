b580e15664bd6bd5d3ac52e0101ea21351065726
John Anderton <janderton24@gmail.com>
2021-02-05 11:16:59 -0700

    adds working solution (400ms) to isPalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,28 @@
+var isPalindrome = function(head) {
+  var f2b = [];
+  var start = head;
+  while (head) {
+      f2b.unshift(head.val);
+      head = head.next;
+  }
+  console.log('f2b: ', f2b);
+  var i = 0;
+
+  head = start;
+
+  while (head) {
+      console.log('head.val: ', head.val);
+      console.log('f2b[', i, ']: ', f2b[i]);
+      if (head.val !== f2b[i]) return false;
+      i++;
+      head = head.next;
+  }
+
+  return true;
+};
+
+/*
+Stats:
+Runtime: 400 ms, faster than 5.23% of JavaScript online submissions for Palindrome Linked List.
+Memory Usage: 51.4 MB, less than 5.09% of JavaScript online submissions for Palindrome Linked List.
+*/
