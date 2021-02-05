26418a0fc6e48f55c4643f17c1de8b8544e480d7
John Anderton <janderton24@gmail.com>
2021-02-05 11:19:23 -0700

    updates solution to run at 216ms

--- isPalindrome.js
+++ isPalindrome.js
@@ -2,11 +2,11 @@ var isPalindrome = function(head) {
   var f2b = [];
   var start = head;
   while (head) {
-      f2b.unshift(head.val);
+      f2b.push(head.val);
       head = head.next;
   }
   console.log('f2b: ', f2b);
-  var i = 0;
+  var i = f2b.length - 1;
 
   head = start;
 
@@ -14,7 +14,7 @@ var isPalindrome = function(head) {
       console.log('head.val: ', head.val);
       console.log('f2b[', i, ']: ', f2b[i]);
       if (head.val !== f2b[i]) return false;
-      i++;
+      i--;
       head = head.next;
   }
 
@@ -23,6 +23,6 @@ var isPalindrome = function(head) {
 
 /*
 Stats:
-Runtime: 400 ms, faster than 5.23% of JavaScript online submissions for Palindrome Linked List.
+Runtime: 216 ms, faster than 5.23% of JavaScript online submissions for Palindrome Linked List.
 Memory Usage: 51.4 MB, less than 5.09% of JavaScript online submissions for Palindrome Linked List.
 */
