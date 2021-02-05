e71aa14b33e8d6284e8404d917f205a759126f1e
John Anderton <janderton24@gmail.com>
2021-02-05 11:21:12 -0700

    removes console logs to bring solution speed down to 88ms

--- isPalindrome.js
+++ isPalindrome.js
@@ -5,14 +5,11 @@ var isPalindrome = function(head) {
       f2b.push(head.val);
       head = head.next;
   }
-  console.log('f2b: ', f2b);
   var i = f2b.length - 1;
 
   head = start;
 
   while (head) {
-      console.log('head.val: ', head.val);
-      console.log('f2b[', i, ']: ', f2b[i]);
       if (head.val !== f2b[i]) return false;
       i--;
       head = head.next;
@@ -23,6 +20,6 @@ var isPalindrome = function(head) {
 
 /*
 Stats:
-Runtime: 216 ms, faster than 5.23% of JavaScript online submissions for Palindrome Linked List.
-Memory Usage: 51.4 MB, less than 5.09% of JavaScript online submissions for Palindrome Linked List.
+Runtime: 88 ms, faster than 58.78% of JavaScript online submissions for Palindrome Linked List.
+Memory Usage: 42.8 MB, less than 43.34% of JavaScript online submissions for Palindrome Linked List.
 */
