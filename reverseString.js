0cac9eb4080255a795b4234ce4f5aef7f64bf51b
John Anderton <janderton24@gmail.com>
2021-02-18 15:52:24 -0700

    adds working solution to reverseString.js with run time of 112 ms

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+// reverseString.js
+
+var reverseString = function(s) {
+  return s.reverse();
+};
+
+/*
+Stats:
+Runtime: 112 ms, faster than 66.42% of JavaScript online submissions for Reverse String.
+Memory Usage: 45.5 MB, less than 89.75% of JavaScript online submissions for Reverse String.
+*/
